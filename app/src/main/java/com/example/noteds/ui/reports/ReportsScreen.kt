675432fa4bb626162633abffc269761b60a81d10
Noyolos <104434080+Noyolos@users.noreply.github.com>
2025-11-21 21:56:14 +0800

    Latest version

--- app/src/main/java/com/example/noteds/ui/reports/ReportsScreen.kt
+++ app/src/main/java/com/example/noteds/ui/reports/ReportsScreen.kt
@@ -19,9 +19,11 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.noteds.ui.theme.*
 import java.text.NumberFormat
 import java.util.Locale
